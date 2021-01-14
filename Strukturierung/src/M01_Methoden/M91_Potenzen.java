840e74cb0f37e9536ed0408560ce94b6000aed9f
Johnny <64163472+J0hnny1@users.noreply.github.com>
2021-01-14 22:40:43 +0100

    Create M91_Potenzen.java
    M01_Potenzen

---
+++ Strukturierung/src/M01_Methoden/M91_Potenzen.java
@@ -0,0 +1,15 @@
+package M01_Methoden;
+
+public class M91_Potenzen {
+    public static void main(String[] args)
+    {
+        System.out.print("Result: ");
+    }
+
+    public static double potenz(double x, double n)
+    {
+        Math.sqrt();
+        double result = 0;
+        return result;
+    }
+}
