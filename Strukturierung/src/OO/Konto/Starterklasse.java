13f653320b5a2828c3e0238328719e1b997f3225
J0hnny1 <googj23@gmail.com>
2021-03-22 08:56:40 +0100

    1.6 Konto

--- Strukturierung/src/OO/Konto/Starterklasse.java
+++ Strukturierung/src/OO/Konto/Starterklasse.java
@@ -2,19 +2,19 @@ package OO.Konto;
 
 public class Starterklasse {
     public static void main(String[] args) {
-        Kunde lukas = new Kunde("DE46S54DS51", 100);
+        Kunde J = new Kunde("DE46S54DS51", 100);
 
-        System.out.println(lukas.getKonto().getKreditLimit());
-        System.out.println(lukas.getKonto().getKontoStand());
+        System.out.println(J.getKonto().getKreditLimit());
+        System.out.println(J.getKonto().getKontoStand());
 
-        lukas.getKonto().einzahlen(100D);
-        System.out.println(lukas.getKonto().getKontoStand());
+        J.getKonto().einzahlen(100D);
+        System.out.println(J.getKonto().getKontoStand());
 
-        lukas.getKonto().auszahlen(25D);
-        System.out.println(lukas.getKonto().getKontoStand());
+        J.getKonto().auszahlen(25D);
+        System.out.println(J.getKonto().getKontoStand());
 
-        lukas.getKonto().auszahlen(250D);
-        System.out.println(lukas.getKonto().getKontoStand());
+        J.getKonto().auszahlen(250D);
+        System.out.println(J.getKonto().getKontoStand());
     }
 }
 
