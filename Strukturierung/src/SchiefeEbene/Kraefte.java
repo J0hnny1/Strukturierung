fb6860f060a22066e1167bea41d35ea7816b85cc
Johnny <64163472+J0hnny1@users.noreply.github.com>
2021-06-07 11:42:04 +0200

    Schiefe Ebene

--- Strukturierung/src/SchiefeEbene/Kraefte.java
+++ Strukturierung/src/SchiefeEbene/Kraefte.java
@@ -3,4 +3,18 @@ package SchiefeEbene;
 public class Kraefte {
     private String name;
     private Vektor wert;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Vektor getWert() {
+        return wert;
+    }
+    public void setWert(Vektor wert) {
+        this.wert = wert;
+    }
 }
--- Strukturierung/src/SchiefeEbene/Starterklasse.java
+++ Strukturierung/src/SchiefeEbene/Starterklasse.java
@@ -2,6 +2,5 @@ package SchiefeEbene;
 
 public class Starterklasse {
     public static void main(String[] args) {
-
     }
 }
